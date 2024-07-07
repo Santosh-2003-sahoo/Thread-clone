3e95fd57aaf1d35aa06f31113304b6aff7aa42dd
SANTOSH SAHOO <101939016+Santosh-2003-sahoo@users.noreply.github.com>
2024-07-07 13:58:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 		// Get rid of the CORS error
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://thread-clone-backend-2og9.onrender.com",
 				changeOrigin: true,
 				secure: false,
 			},
